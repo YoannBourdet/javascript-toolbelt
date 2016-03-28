8e00bea5cca0c355b5e7f2b9de26bf4c12123fe7
Yoann Bourdet <yoannbourdet@gmail.com>
2016-03-28 22:44:02 +0200

    add object functions: iterate, map, values, keys...

--- src/object.js
+++ src/object.js
@@ -1,6 +1,45 @@
-/*
-  eslint
-  no-unused-vars: 0
-*/
 
-export class Object {}
+export function * iterate(obj) {
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      yield { key, val: obj[key] };
+    }
+  }
+}
+
+export function values(obj) {
+  const newArray = [];
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      newArray.push(key);
+    }
+  }
+  return newArray;
+}
+
+export function keys(obj) {
+  const newArray = [];
+  for (const { key } of iterate(obj)) {
+    newArray.push(key);
+  }
+  return newArray;
+}
+
+export function map(obj, cb) {
+  const newObject = {};
+  for (const { entry } of iterate(obj)) {
+    if (cb) {
+      const { key, val } = cb(entry);
+      newObject[key] = val;
+    }
+  }
+  return newObject;
+}
+
+export function clone(obj) {
+  return Object.assign({}, obj);
+}
+
+export function isObject(obj) {
+  return typeof obj === 'object' || obj instanceof Object || typeof obj === 'function';
+}
