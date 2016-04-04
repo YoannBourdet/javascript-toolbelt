c00e4b4c421f19ce2e3b8dbcefd683a2b34f596a
Yoann Bourdet <yoannbourdet@gmail.com>
2016-04-04 22:27:24 +0200

    add array function.

--- src/array.js
+++ src/array.js
@@ -2,3 +2,43 @@
   eslint
   no-unused-vars: 0
 */
+
+export const getFirst = (arr, n = 1) => {
+  const newArr = n < 0 ? arr.slice(0, n) : arr.slice(0, Math.abs(n));
+  return newArr;
+};
+
+export const getLast = (arr, n = 1) => {
+  const newArr = n < 0 ? arr.slice(+`-${n}`) : arr.slice(0, n);
+  return newArr;
+};
+
+export const ignoreLast = (arr, n = 1) => {
+  const newArr = n < 0 ? arr.slice(0, +`-${n}`) : arr.slice(0, n);
+  return newArr;
+};
+
+export const ignoreFirst = (arr, n = 1) => {
+  const newArr = n < 0 ? arr.slice(n) : arr.slice(Math.abs(n));
+  return newArr;
+};
+
+export const compact = (arr) => {
+  const newArr = [];
+  arr.forEach((el) => {
+    if (el) {
+      newArr.push(el);
+    }
+  });
+  return newArr;
+};
+
+export const removeDoubleKeys = (arr) => {
+  const newArr = [];
+  arr.forEach((el) => {
+    if (!newArr.includes(el)) {
+      newArr.push(el);
+    }
+  });
+  return newArr;
+};
